9076dd7d04865913a9cd1d6034a216ad77bf5fcc
JNeaderland <144148081+JNeaderland@users.noreply.github.com>
2023-10-02 14:11:03 -0400

    Add files via upload

---
+++ Lab 3-6.py
@@ -0,0 +1,16 @@
+#Author:Jacob Neaderland
+string = "Fairfield Prep"
+a = "--------------------"
+print (a)
+print (len (a))
+
+b = "I'm loving this short story I'm reading, \"The Fall of the House of Usher\""
+print (b)
+print (len (b))
+
+c = "apple"
+print (c[3])
+if "l" in  c:
+    print ("L")
+else:
+    print ("?")
